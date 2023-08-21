9a938446d919b8de3d8c7ba96cc3922d51356ef8
Novaturiente <novaturiente@proton.me>
2023-08-21 11:05:24 +0530

    setup.sh update

--- configs/setup.sh
+++ configs/setup.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-
 #Installing Packages
 yes | sudo pacman -S --needed base-devel git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 yes | makepkg -si
 
-yay -S neovim -y
-yay -S hyprland-nvidia waybar rofi alacritty hyprpaper brightnessctl -y
-yay -S zsh -y
-yay -S flatpak -y
-yay -S ttf-space-mono-nerd ttf-nerd-fonts-symbols-common otf-sora otf-firamono-nerd ttf-nerd-fonts-symbols-common ttf-iosevka-nerd -y
-
-yay -S mullvad-browser-bin -y
 #Copying configuration
 cd ~/.config
 
@@ -29,3 +21,9 @@ ln -s ~/Configs/alacritty
 ln -s ~/Configs/hypr
 
 ####EXTRAS
+yay -S neovim -y
+yay -S hyprland-nvidia waybar rofi alacritty hyprpaper brightnessctl -y
+yay -S zsh -y
+yay -S flatpak -y
+yay -S ttf-space-mono-nerd ttf-nerd-fonts-symbols-common otf-sora otf-firamono-nerd ttf-nerd-fonts-symbols-common ttf-iosevka-nerd -y
+yay -S mullvad-browser-bin -y
